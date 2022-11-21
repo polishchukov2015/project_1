f634af5d6d45898845b0a06107cfbff27962610d
olyapolishchuk <polishchukov2015@gmail.co>
2022-11-21 11:32:09 +0200

    first coomit

---
+++ src/main/java/com/portfolio/project_1/controller/MessageController.java
@@ -0,0 +1,9 @@
+package com.portfolio.project_1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("message")
+public class MessageController {
+}
